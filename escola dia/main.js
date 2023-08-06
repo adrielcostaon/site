69c4cbf19cdefa05ddc8afef886b2e90d789e5e9
Adriel de Campos Costa <104805021+adrielcostaon@users.noreply.github.com>
2023-08-06 15:30:40 -0300

    Add files via upload

---
+++ escola dia/main.js
@@ -0,0 +1,18 @@
+function getProjects(){
+    const urlGitHub = 'https://github.com/adrielcostaon'
+    var loadingElement = document.getElementById('loading')
+
+    fetch(urlGitHub,{
+        method: 'GET',
+    })
+        .then((response) => response.json())
+        .then((response) => {
+            console.log(response)
+            showProjects(response)
+            loadingElement.style.display = 'none'
+        })
+        .catch((e) => {
+            console.log(`Error -> ${e}`)
+        })
+}
+
